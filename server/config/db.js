ec89c477eb0f11d3bbfd203f98b600c506961ebb
Sonal Singh <sonal1201kumar@gmail.com>
2025-10-03 17:56:32 +0530

    Feat

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-const { neon } = require('@neondatabase/serverless');
+import { neon } from '@neondatabase/serverless';
 
 
 const sql = neon(`${process.env.DATABASE_URL}`)
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,15 +1,18 @@
 import { clerkClient } from "@clerk/express";
 
+// Middleware to check userID and hasPremiumPlan
+
 export const auth = async (req, res, next) => {
   try {
-    const { user_id, has } = await req.auth();
+    const { userId, has } = await req.auth();
     const hasPremiumPlan = await has({ plan: "premium" });
-    const user = await clerkClient.users.getUser(user_id);
+
+    const user = await clerkClient.users.getUser(userId);
 
     if (!hasPremiumPlan && user.privateMetadata.free_usage) {
       req.free_usage = user.privateMetadata.free_usage;
     } else {
-      await clerkClient.users.updateUserMetadata(user_id, {
+      await clerkClient.users.updateUserMetadata(userId, {
         privateMetadata: {
           free_usage: 0,
         },
@@ -17,10 +20,8 @@ export const auth = async (req, res, next) => {
       req.free_usage = 0;
     }
     req.plan = hasPremiumPlan ? "premium" : "free";
+    next();
   } catch (error) {
-    res.json({
-        success: false,
-        message: error.message
-    })
+    res.json({ success: false, message: error.message });
   }
 };
